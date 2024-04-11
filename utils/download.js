459d99228b18cb8eab48d5477ba5ab17cf5d8957
njasfoods <notjusasalad@gmail.com>
2024-04-10 21:53:39 -0500

    first commit

--- utils/download.js
+++ utils/download.js
@@ -1,10 +1,16 @@
-// utils/youtubeDownloader.js
+'use server'
 
 import fs from 'fs';
+import path from 'path';
 import ytdl from 'ytdl-core';
 
-async function downloadYouTubeVideo(videoUrl, outputPath) {
+async function downloadYouTubeVideo(videoUrl) {
   try {
+    const videoInfo = await ytdl.getInfo(videoUrl); // Fetch metadata about the video
+    const videoTitle = videoInfo.videoDetails.title; // Get the title of the video
+    const filename = `${videoTitle}.mp4`; // Construct filename using the video title
+    const outputPath = path.join(__dirname, filename);
+    console.log(outputPath)
     // Download video
     await new Promise((resolve, reject) => {
       const videoStream = ytdl(videoUrl, { filter: 'audioandvideo' });
@@ -17,10 +23,10 @@ async function downloadYouTubeVideo(videoUrl, outputPath) {
     });
 
     console.log('Video downloaded successfully!');
-    return true;
+    return filename; // Return the filename
   } catch (error) {
     console.error('Error downloading video:', error);
-    return false;
+    return null;
   }
 }
 
